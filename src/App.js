91517eb0d39425db7c21613e7f06ffc1fdf88062
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-24 02:24:37 +0600

    banner part add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
