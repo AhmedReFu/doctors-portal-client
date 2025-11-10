99d69296aed6bb76e7a92325fca5bc0ec4c37418
MD NAZMUL ISLAM <59439125+AhmedReFu@users.noreply.github.com>
2025-11-11 02:42:25 +0600

    Update admin fetch URL to production server

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -6,7 +6,7 @@ const useAdmin = user => {
     useEffect(() => {
         const email = user?.email;
         if (email) {
-            fetch(`http://localhost:5001/admin/${email}`, {
+            fetch(`https://doctors-protal-server-delta.vercel.app/admin/${email}`, {
                 method: 'GET',
                 headers: {
                     'content-type': 'application/json',
