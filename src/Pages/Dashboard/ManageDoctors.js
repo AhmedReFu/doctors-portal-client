69b433130c258d4a0a0ca58c761ef5d7d52320db
MD NAZMUL ISLAM <59439125+AhmedReFu@users.noreply.github.com>
2025-11-11 02:45:49 +0600

    Update ManageDoctors.js

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -6,7 +6,7 @@ import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
     const [deletingDoctor, setDeletingDoctor] = useState(null)
-    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:5001/doctor', {
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('https://doctors-protal-server-delta.vercel.app/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
