0b8d39c0bdfc4fa28348b53211671311bd8b9cf6
MD NAZMUL ISLAM <59439125+AhmedReFu@users.noreply.github.com>
2025-11-11 02:42:12 +0600

    Update useToken.js

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -6,7 +6,7 @@ const useToken = user => {
         const email = user?.user?.email;
         const currentUser = { email: email };
         if (email) {
-            fetch(`http://localhost:5001/user/${email}`, {
+            fetch(`https://doctors-protal-server-delta.vercel.app/user/${email}`, {
                 method: 'PUT',
                 headers: {
                     'content-type': 'application/json'
